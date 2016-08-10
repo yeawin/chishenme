5e2208d57b1af791b74d594c84be4ef4ee29ce11
yeawin <ywliu@xmu.edu.cn>
2016-08-10 18:35:38 +0800

    读取数据

--- chishenme/application/controllers/IndexController.php
+++ chishenme/application/controllers/IndexController.php
@@ -11,6 +11,14 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $Huncai = new Application_Model_Huncai();
+        $huncai = $Huncai->get_list();
+        $this->view->headScript()->appendScript("var huncai = " . json_encode($huncai).";", $type = 'text/javascript');
+        
+        $Sucai = new Application_Model_Sucai();
+        $sucai = $Sucai->get_list();
+//         $this->view->list = $sucai;
+        $this->view->headScript()->appendScript("var sucai = " . json_encode($sucai).";", $type = 'text/javascript');
     }
 
 
