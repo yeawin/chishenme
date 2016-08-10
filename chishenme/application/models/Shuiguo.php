83405d78bfd8940441bcf4c00e0e1cbaeec6b5a6
yeawin <ywliu@xmu.edu.cn>
2016-08-10 17:49:24 +0800

    数据模型

---
+++ chishenme/application/models/Shuiguo.php
@@ -0,0 +1,41 @@
+<?php
+
+class Application_Model_Shuiguo extends Zend_Db_Table_Abstract
+{
+
+    protected $_primary = "id";
+    
+    protected $_name = "shuiguo";
+     
+    protected $_id;
+    
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+
+    public function get_list()
+    {
+        $select = $this->select();
+        $select->from($this->_name);
+        $result = $this->fetchAll($select);
+        if ($result) {
+            $result = $result->toArray();
+        }
+        return $result;
+    }
+    
+    public function insert_record($data)
+    {
+        return $this->insert($data);
+    }
+    
+    public function update_record($data, $id)
+    {
+        $db = $this->getDefaultAdapter();
+        $where = $db->quoteInto("id = ?", $id);
+        return $this->update($data, $where);
+    }
+
+}
+
