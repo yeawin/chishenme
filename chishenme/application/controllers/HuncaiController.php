fe8e475ffef2796856a8c5f0a7cba53739d8a354
yeawin <ywliu@xmu.edu.cn>
2016-08-10 18:11:48 +0800

    huncai list

--- chishenme/application/controllers/HuncaiController.php
+++ chishenme/application/controllers/HuncaiController.php
@@ -11,6 +11,9 @@ class HuncaiController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $Huncai = new Application_Model_Huncai();
+        $huncai = $Huncai->get_list();
+        $this->view->list = $huncai;
     }
 
 
